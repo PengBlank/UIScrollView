dc3f4ddab8f13b696aed0cf43a405e5531a672a1
peng dong <RayXiang@123.local>
2016-05-06 19:42:50 +0800

    go on at home

--- scrollview/ViewController.m
+++ scrollview/ViewController.m
@@ -12,6 +12,7 @@
 
 @property(nonatomic, strong)UIScrollView *myScrollView;
 @property(nonatomic, strong)UIPageControl *myPageControl;
+@property(nonatomic, assign)NSInteger currentPage;
 @property(nonatomic, strong)NSTimer *myTimer;
 
 @end
@@ -35,7 +36,7 @@
     _myScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, 250)];
     _myScrollView.backgroundColor = [UIColor redColor];
     _myScrollView.pagingEnabled = YES;
-    _myScrollView.contentSize = CGSizeMake(self.view.frame.size.width * 3, self.view.frame.size.height) ;
+    _myScrollView.contentSize = CGSizeMake(self.view.frame.size.width * 3, self.myScrollView.frame.size.height) ;
     self.myScrollView.delegate = self;
     [self.view addSubview:_myScrollView];
 }
@@ -45,6 +46,7 @@
                       CGRectMake((_myScrollView.frame.size.width - 160)/2 , _myScrollView.frame.size.height - 16, 160, 16)];
     _myPageControl.numberOfPages = 4;
     _myPageControl.currentPage = 0;
+    self.currentPage = 0;
     self.myPageControl.currentPageIndicatorTintColor=[UIColor yellowColor];
     self.myPageControl.pageIndicatorTintColor = [UIColor grayColor];
     [self.view addSubview:_myPageControl];
@@ -52,7 +54,7 @@
 
 
 -(void)loadTimer{
-    _myTimer = [NSTimer timerWithTimeInterval:2.0 target:self selector:@selector(changeImage:) userInfo:nil repeats:YES];
+    _myTimer = [NSTimer timerWithTimeInterval:3 target:self selector:@selector(changeImage:) userInfo:nil repeats:YES];
     [[NSRunLoop currentRunLoop] addTimer:_myTimer forMode:NSDefaultRunLoopMode];
 }
 
@@ -61,7 +63,7 @@
     
     if (!_myScrollView.isDragging)
     {
-        CGPoint newOffset = CGPointMake(_myScrollView.contentOffset.x + CGRectGetWidth(_myScrollView.frame), _myScrollView.contentOffset.y);
+        CGPoint newOffset = CGPointMake(CGRectGetWidth(_myScrollView.frame), _myScrollView.contentOffset.y);
         
         if ((int)newOffset.x%(int)CGRectGetWidth(_myScrollView.frame) != 0)
         {
@@ -69,10 +71,12 @@
         };
         
         [_myScrollView setContentOffset:newOffset animated:YES];
-        self.myPageControl.currentPage = newOffset.x / self.myScrollView.frame.size.width;
+//        self.myPageControl.currentPage = newOffset.x / self.myScrollView.frame.size.width;
+//        [self getCurrentImage:(newOffset.x / self.myScrollView.frame.size.width) ];
+//        [self loadData];
         //DebugNSLog(@"autoPlayAds %@", self);
     }
-}
+} 
 
 -(void)loadData{
     
@@ -89,10 +93,10 @@
         v.clipsToBounds = YES;
         [_myScrollView addSubview:v];
     }
-    
-    
+     [_myScrollView setContentOffset:CGPointMake(_myScrollView.frame.size.width, 0) animated:NO];
+//    self.myPageControl.currentPage = 0;
 //    CGPoint offset = self.myScrollView.contentOffset;
-//    
+//
 //    if (pageNum >= 3) {
 //        pageNum = 0;
 //        offset.x = 0;
@@ -126,12 +130,54 @@
 
 -(UIView *) loadImageWithPageNum:(NSInteger )pageNum{
     UIImage *image = [UIImage imageNamed: [NSString stringWithFormat:@"h%ld.jpg",(long)pageNum + 1]];
-    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, CGRectGetHeight(self.view.frame))];
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, CGRectGetHeight(self.myScrollView.frame))];
     imageView.image = image;
     return imageView;
 }
 
 #pragma scroll View Delegate
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    //如果scrollView当前偏移位置x大于等于两倍scrollView宽度
+    if (_myScrollView.contentOffset.x >= self.myScrollView.frame.size.width * 2) {
+        //当前图片位置+1
+        _currentPage++;
+        //如果当前图片位置超过数组边界，则设置为0
+//        if (_currentPage == [self.imageArray count]) {
+//            _currentPage = 0;
+//        }
+        [self getCurrentImage:self.currentPage];
+        //刷新图片
+        [self loadData];
+        //设置scrollView偏移位置
+//        [_myScrollView setContentOffset:CGPointMake(c_width, 0)];
+    }
+    
+    //如果scrollView当前偏移位置x小于等于0
+    else if (_myScrollView.contentOffset.x <= 0) {
+        //当前图片位置-1
+        _currentPage--;
+        [self getCurrentImage:_currentPage];
+        //如果当前图片位置小于数组边界，则设置为数组最后一张图片下标
+//        if (_currentPage == -1) {
+//            _currentPage = [self.imageArray count]-1;
+//        }
+        //刷新图片
+        [self loadData];
+        //设置scrollView偏移位置
+//        [_myScrollView setContentOffset:CGPointMake(c_width, 0)];
+    }
+}
+
+
+
+
+
+
+
+
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
     //使定时器失效
     [self.myTimer invalidate];
